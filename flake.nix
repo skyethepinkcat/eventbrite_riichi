daff4006edc5b0b23d0e442185cdff310af8c9d5
Skye J <skye@skyenet.online>
2026-08-29 13:26:32 -0400

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
       in rec {
         defaultPackage = naersk'.buildPackage {
           src = ./.;
+          meta.mainProgram = "eventbrite_riichi";
         };
 
         devShell = pkgs.mkShell {
