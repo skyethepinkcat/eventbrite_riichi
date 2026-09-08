4da76ec1aca7525ae6ac50debd7bbe95b24f22b4
Skye J <skye@skyenet.online>
2026-09-08 15:52:34 -0400

    feat: use crane flake

--- flake.nix
+++ flake.nix
@@ -1,143 +1,23 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    crane.url = "github:ipetkov/crane";
+  description = "Single crate example for rust-flake";
 
-    advisory-db = {
-      url = "github:rustsec/advisory-db";
-      flake = false;
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-flake.url = "github:juspay/rust-flake";
   };
 
-  outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-      crane,
-      advisory-db,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) lib;
-        craneLib = crane.mkLib pkgs;
-        src = craneLib.cleanCargoSource ./.;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-
-          buildInputs = [
-            # Add additional build inputs here
-          ]
-          ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
-        };
-
-        # Build *just* the cargo dependencies (of the entire workspace),
-        # so we can reuse all of that work (e.g. via cachix) when running in CI
-        # It is *highly* recommended to use something like cargo-hakari to avoid
-        # cache misses when building individual top-level-crates
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        eventbrite_riichi = craneLib.buildPackage (
-
-          commonArgs
-          // {
-            src = craneLib.cleanCargoSource ./.;
-            inherit cargoArtifacts;
-
-            # Add extra inputs here or any other derivation settings
-            # doCheck = true;
-            # buildInputs = [];
-            # nativeBuildInputs = [];
-          }
-        );
-      in
-      {
-        checks = {
-          inherit eventbrite_riichi;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          eventbrite_riichi-clippy = craneLib.cargoClippy (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-            }
-          );
-
-          eventbrite_riichi-doc = craneLib.cargoDoc (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              # This can be commented out or tweaked as necessary, e.g. set to
-              # `--deny rustdoc::broken-intra-doc-links` to only enforce that lint
-              env.RUSTDOCFLAGS = "--deny warnings";
-            }
-          );
-
-          # Check formatting
-          eventbrite_riichi-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          eventbrite_riichi-toml-fmt = craneLib.taploFmt {
-            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-            # taplo arguments can be further customized below as needed
-            # taploExtraArgs = "--config ./taplo.toml";
-          };
-
-          # Audit dependencies
-          eventbrite_riichi-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-
-          # Audit licenses
-          eventbrite_riichi-deny = craneLib.cargoDeny {
-            inherit src;
-          };
-
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `eventbrite_riichi` if you do not want
-          # the tests to run twice
-          eventbrite_riichi-nextest = craneLib.cargoNextest (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              partitions = 1;
-              partitionType = "count";
-              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
-            }
-          );
-
-        };
-        packages.default = eventbrite_riichi;
-        devShells.default = craneLib.devShell {
-          checks = self.checks.${system};
-          packages = with pkgs; [
-            rust-analyzer
-
-          ];
-
-          # Automatically inherit any build inputs from `my-crate`
-          inputsFrom = [ eventbrite_riichi ];
-        };
-      }
-    );
+      perSystem = { self', ... }: {
+        devShells.default = self'.devShells.rust;
+        packages.default = self'.packages.eventbrite_riichi;
+      };
+    };
 }
